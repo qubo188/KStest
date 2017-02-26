f4312f05320abd84f8088ce103495094a2e16e33
jiaojian <jiaojian@qq.com>
2017-02-26 22:42:04 +0800

    jiaojian commit

--- src/main/java/com/qubo/Controller/MainController.java
+++ src/main/java/com/qubo/Controller/MainController.java
@@ -8,4 +8,10 @@ public class MainController {
     public static void main(String[] args) {
         System.out.println("张三");
     }
+    public void jiaojian_method01(){
+
+    }
+    public void jiaojian_method02(){
+
+    }
 }
