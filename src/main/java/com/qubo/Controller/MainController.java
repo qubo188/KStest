2a39e657c49d606a203eddc1f7305a784ac6ce49
qb <1411191271@qq.com>
2017-02-26 22:24:01 +0800

    qb commit MainController

--- src/main/java/com/qubo/Controller/MainController.java
+++ src/main/java/com/qubo/Controller/MainController.java
@@ -8,4 +8,12 @@ public class MainController {
     public static void main(String[] args) {
         System.out.println("张三");
     }
+
+    public void method01(){
+
+    }
+    public void method02(){
+
+
+    }
 }
