3e9e69409bd0cee4726e2bf6af3ff27b9a57e902
qb <1411191271@qq.com>
2017-02-26 22:09:10 +0800

    qb create MainController

---
+++ src/main/java/com/qubo/Controller/MainController.java
@@ -0,0 +1,11 @@
+package com.qubo.Controller;
+
+/**
+ * Created by Administrator on 2017/2/26 0026.
+ */
+public class MainController {
+
+    public static void main(String[] args) {
+        System.out.println("张三");
+    }
+}
